e6da333f39e39e33aaf32e6b4bb522c69d7fd444
Stefan Stefanov <stefan.bg@gmail.com>
2016-08-13 09:43:48 +0300

    Eslint configuration as a node module

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+    extends: 'google',
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true
+    },
+    installedESLint: true,
+    parserOptions: {
+        sourceType: "module"
+    },
+    rules: {
+        "indent": ["error", 4, {SwitchCase: 1}],
+        "linebreak-style": ["error", "unix"],
+        "semi": ["error", "always"],
+        "max-len": [1, {code: 120}],
+        "no-unused-expressions": [0],
+        "new-cap": [0],
+        "generator-star-spacing": ["off"],
+        "space-before-function-paren": ["off"],
+        "no-eq-null": ["off"],
+        "eqeqeq": ["off"],
+        "one-var": ["off"],
+        "quote-props": ["off"]
+    }
+};
